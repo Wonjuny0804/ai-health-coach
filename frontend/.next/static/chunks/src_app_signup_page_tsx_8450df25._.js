845e00a9ae5e90436d7d9a6c9e802df6f8104a02
Wonjun <wonwonjun@gmail.com>
2025-06-12 10:54:06 -0500

    Modify gitignore

--- frontend/.next/static/chunks/src_app_signup_page_tsx_8450df25._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@supabase_node-fetch_browser_9aa21661.js",
-  "static/chunks/node_modules_cd69ceeb._.js",
-  "static/chunks/src_54d480a1._.js"
-],
-    source: "dynamic"
-});
